5dd4306d0192284a6c9fb78329e0b1ac6b1f0449
Claudia Quintana Wong <cquintana@alumni.afi.es>
2021-02-18 20:59:25 +0100

    applying clustering

--- clustering_census.R
+++ clustering_census.R
@@ -1,3 +1,6 @@
+library(cluster)
+library(vegan)
+
 census <- read.csv(file = 'data/census2000_conLongitudLatitud.csv')
 summary(census)
 
@@ -5,8 +8,19 @@ census$MedHHInc <- as.numeric(gsub('[$,]', '', census$MedHHInc))
 census$MeanHHSz <- as.numeric(gsub('[$,]', '', census$MeanHHSz))
 census$RegPop <- as.numeric(gsub('[,]', '', census$RegPop))
 
+
+
+
+#removing location columns
 census_data <- census[c(-1, -2, -3)]
-census_data$RegDens <- log(census_data$RegDens-min(census_data$RegDens)+1)
+
+#estandarizando los datos
+#census_data$RegDens <- log(census_data$RegDens-min(census_data$RegDens)+1)
 census_data$RegPop <- log(census_data$RegPop-min(census_data$RegPop)+1)
 census_data$MedHHInc <- log(census_data$MedHHInc-min(census_data$MedHHInc)+1)
 census_data$MeanHHSz <- log(census_data$MeanHHSz-min(census_data$MeanHHSz)+1)
+summary(census_data)
+
+matrizDistancias <- vegdist(census_data, method = "euclidean", na.rm = TRUE)
+clusterJerarquico <- hclust(matrizDistancias, method="ward.D2") 
+graphics.off()
